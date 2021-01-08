4ca4e29ff9532f07b7786c3f0c399eb62728981d
lyfhouyi <lyfhouyi@163.com>
2021-01-08 23:49:35 +0800

    houyi

--- letcode/189_Medium.cpp
+++ letcode/189_Medium.cpp
@@ -61,54 +61,10 @@ using namespace std;
 //}
 
 
-//迭代。瞄准起始时数组尾部的 k 个元素作为“目标段”，每次循环将其与左侧的 k 个元素交换位置，一共循环 (n-1)/k 次。
-//算法正确，时间超时。时间复杂度 O(n)，时间复杂度 O(1)。
-//void rotate(vector<int>& nums, int k)
-//{
-//	int size = nums.size();
-//	if (size <= 1)
-//		return;
-//	k = k % size;
-//	if (0 == k)
-//		return;
-//	int i;
-//	int j;
-//	int sour=size+k;
-//	int dist = 0;
-//
-//	//前 (n-1)/k-1 次正常循环
-//	for (i = 0; i < (size - 1) / k-1;i++)
-//	{
-//		sour -= 2*k;
-//		dist = sour - k;
-//		for (j = 0; j < k; j++)
-//		{
-//			swap(nums[sour], nums[dist]);
-//			sour++;
-//			dist = sour - k;
-//		}
-//	}
-//	//最后一次循环只需在 [begin,目标段尾] 间循环
-//	sour -= 2*k;
-//	size = sour + k;
-//	int tmp;
-//	vector<int>::iterator it = nums.begin()+size;
-//	while (it != nums.begin() + k)
-//	{
-//		it = it - k;
-//		tmp = *(it - 1);
-//		for (i = 0; i < k; i++)
-//		{
-//			*(it - 1) = *it;
-//			it++;
-//		}
-//		it--;
-//		*it = tmp;
-//	}
-//}
 
 
-//使用额外的空间
+
+//解法一：使用额外的空间
 //void rotate(vector<int>& nums, int k)
 //{
 //	int size = nums.size();
@@ -119,22 +75,49 @@ using namespace std;
 //}
 
 
-//数组翻转
+//解法二：数组翻转
+//void rotate(vector<int>& nums, int k)
+//{
+//	k = k % nums.size();
+//	reverse(nums.begin(), nums.end());
+//	reverse(nums.begin(), nums.begin() + k);
+//	reverse(nums.begin() + k, nums.end());
+//}
+
+
+//解法三：环状替换
 void rotate(vector<int>& nums, int k)
 {
-	k = k % nums.size();
-	reverse(nums.begin(), nums.end());
-	reverse(nums.begin(), nums.begin() + k);
-	reverse(nums.begin() + k, nums.end());
+	int size = nums.size();
+	k = k % size;
+	int changeCnt = 0;
+	int startIndex=0;
+	int currentIndex;
+	int changeIndex;
+	int tmp;
+	while (changeCnt < size)
+	{
+		currentIndex = startIndex;
+		tmp = nums[currentIndex];
+		do 
+		{
+			changeIndex = (currentIndex + k + size) % size;
+			swap(nums[changeIndex], tmp);
+			changeCnt++;
+			currentIndex = changeIndex;
+		} while (currentIndex != startIndex);
+		startIndex++;
+	}
 }
 
-int main()
+
+int main189()
 {
-	vector<int> test = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 };
+	vector<int> test = { 1};
 	for (vector<int>::iterator it = test.begin(); it != test.end(); it++)
 		cout << *it << "  ";
 	cout << endl;
-	rotate(test, 0);
+	rotate(test,7);
 	for (vector<int>::iterator it = test.begin(); it != test.end(); it++)
 		cout << *it << "  ";
 	cout << endl;
---
+++ letcode/5_Medium.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//给你一个字符串 s，找到 s 中最长的回文子串。
+//
+//
+//提示：
+//
+//	1 <= s.length <= 1000
+//	s 仅由数字和英文字母（大写和 / 或小写）组成
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/longest-palindromic-substring
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+//使用 Manacher 算法求得最大回文半径数组，基于最大回文半径数组找到最长的回文子串。
+string longestPalindrome(string s)
+{
+	// Manacher 算法
+	//初始化母串
+	string Ms = "$#";
+	for (string::iterator it = s.begin(); it != s.end(); it++)
+	{
+		Ms += *it;
+		Ms += '#';
+	}
+	Ms += "!";
+	//cout << s << endl;
+	//cout << Ms << endl;
+
+	int MsSize = Ms.size();
+	vector<int> rMaxArray(MsSize, 1); //各位置最大回文半径初始化为 1
+
+	int r = 0;
+	int rMax = 1; //最大回文右端点
+	int rCentre = 1; //最大回文右端点对应的回文中心
+	int j = 0;
+	//int cnt = 0;
+
+	for (int i = 1; i < MsSize; i++)
+	{
+		j = 2 * rCentre - i;
+		r = i < rMax ? min(rMaxArray[j] - 1, rMax - i) : 1;
+		while (Ms[i - r] == Ms[i + r])
+		{
+			//if (1 == r % 2)
+			//	cout << "countSubstrings：找到回文子串  " << ++cnt << "  " << string(Ms.begin() + i - r, Ms.begin() + i + r + 1) << "长度="<<r<<endl;
+			r++;
+		}
+		rMaxArray[i] = r;
+		rCentre = i + r - 1 > rMax ? i : rCentre;
+		rMax = i + r - 1 > rMax ? i + r - 1 : rMax;
+	}
+
+	//查找最长回文子串
+	vector<int>::iterator longestPos = max_element(rMaxArray.begin(), rMaxArray.end());
+	int longestIndex = longestPos - rMaxArray.begin();
+	string ret;
+	for (string::iterator it = Ms.begin() + longestIndex - *longestPos + 1; it != Ms.begin() + longestIndex + *longestPos; it++)
+	{
+		if (*it != '#')
+			ret += *it;
+	}
+	return ret;
+}
+
+
+int main()
+{
+	string test = "abba";
+	string ret = longestPalindrome(test);
+	cout << "main：ret = " << ret << endl;
+	return 0;
+}
