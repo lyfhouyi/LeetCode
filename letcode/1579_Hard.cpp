1e91a315e0f49173a4c5949cf617db7c5ab61458
lyfhouyi <lyfhouyi@163.com>
2021-01-27 21:18:34 +0800

    houyi

---
+++ letcode/1579_Hard.cpp
@@ -0,0 +1,160 @@
+﻿#include<iostream>
+#include<vector>
+//#include<algorithm>
+
+using namespace std;
+
+//Alice 和 Bob 共有一个无向图，其中包含 n 个节点和 3  种类型的边：
+//
+//类型 1：只能由 Alice 遍历。
+//类型 2：只能由 Bob 遍历。
+//类型 3：Alice 和 Bob 都可以遍历。
+//给你一个数组 edges ，其中 edges[i] = [typei, ui, vi] 表示节点 ui 和 vi 之间存在类型为 typei 的双向边。请你在保证图仍能够被 Alice和 Bob 完全遍历的前提下，找出可以删除的最大边数。如果从任何节点开始，Alice 和 Bob 都可以到达所有其他节点，则认为图是可以完全遍历的。
+//
+//返回可以删除的最大边数，如果 Alice 和 Bob 无法完全遍历图，则返回 - 1 。
+//
+//
+//提示：
+//
+//	1 <= n <= 10 ^ 5
+//	1 <= edges.length <= min(10 ^ 5, 3 * n * (n - 1) / 2)
+//	edges[i].length == 3
+//	1 <= edges[i][0] <= 3
+//	1 <= edges[i][1] < edges[i][2] <= n
+//	所有元组(typei, ui, vi) 互不相同
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+class UnionFind1579
+{
+public:
+	UnionFind1579(int n) : fa(vector<int>(n)), domainCnt(n), rank(vector<int>(n)) { init(n); }
+	UnionFind1579(UnionFind1579& unionFind) :fa(unionFind.fa), domainCnt(unionFind.domainCnt), rank(unionFind.rank) {}
+
+	void init(int n) //初始化，每个位置一个集合，位置自己是自己集合的代表元，每个集合的秩都为 1
+	{
+		for (int i = 0; i < n; i++)
+		{
+			fa[i] = i;
+			rank[i] = 1;
+		}
+
+	}
+
+	int find(int index) //查找（路径压缩），找到位置 index 所在集合的代表元
+	{
+		if (index == fa[index])
+			return index;
+		else
+		{
+			fa[index] = find(fa[index]);
+			return fa[index];
+		}
+	}
+
+	void merge(int indexI, int indexJ) //合并（按秩合并）
+	{
+		int rootI = find(indexI);
+		int rootJ = find(indexJ);
+		if (rootI == rootJ)
+			return;
+		if (rank[rootI] < rank[rootJ])
+			fa[rootI] = rootJ;
+		else
+			fa[rootJ] = rootI;
+		if (rank[rootI] == rank[rootJ])
+			rank[rootJ]++;
+		domainCnt--;
+	}
+
+	int getDomainCnt()
+	{
+		return domainCnt;
+	}
+private:
+	vector<int>fa; //父节点数组
+	vector<int>rank; //秩数组
+	int domainCnt;
+};
+
+
+int maxNumEdgesToRemove(int n, vector<vector<int>>& edges)
+{
+	int m = edges.size();
+	int removeCnt = 0;
+	UnionFind1579 unionFind(n);
+
+	//使用公共边（类型3）连接节点
+	for (int i = 0; i < m; i++)
+	{
+		if (3 == edges[i][0])
+		{
+			if (unionFind.find(edges[i][1] - 1) == unionFind.find(edges[i][2] - 1))
+				removeCnt++;
+			else
+				unionFind.merge(edges[i][1] - 1, edges[i][2] - 1);
+		}
+	}
+
+	if (1 == unionFind.getDomainCnt())
+	{
+		//cout << "maxNumEdgesToRemove：公共边全连通" << endl;
+		return m - n + 1; //理想情况下，只需 n - 1 条公共边即可实现全图连通
+	}
+
+	UnionFind1579 unionFind_duplicate(unionFind);
+
+	//使用 Alice 的边连通 Alice 的域
+	for (int i = 0; i < m; i++)
+	{
+		if (1 == edges[i][0])
+		{
+			if (unionFind.find(edges[i][1] - 1) == unionFind.find(edges[i][2] - 1))
+				removeCnt++;
+			else
+				unionFind.merge(edges[i][1] - 1, edges[i][2] - 1);
+		}
+	}
+
+	if (1 != unionFind.getDomainCnt())
+	{
+		//cout << "maxNumEdgesToRemove：Alice 无法连通" << endl;
+		return -1;
+	}
+
+	//使用 Bob 的边连通 Bob 的域
+	for (int i = 0; i < m; i++)
+	{
+		if (2 == edges[i][0])
+		{
+			if (unionFind_duplicate.find(edges[i][1] - 1) == unionFind_duplicate.find(edges[i][2] - 1))
+				removeCnt++;
+			else
+				unionFind_duplicate.merge(edges[i][1] - 1, edges[i][2] - 1);
+		}
+	}
+
+	if (1 != unionFind_duplicate.getDomainCnt())
+	{
+		//cout << "maxNumEdgesToRemove：Bob 无法连通" << endl;
+		return -1;
+	}
+
+	//cout << "maxNumEdgesToRemove：删除完毕" << endl;
+	return removeCnt;
+}
+
+
+int main()
+{
+	//vector<vector<int>> test = { {3, 1, 2},{3, 2, 3},{1, 1, 3},{1, 2, 4},{1, 1, 2},{2, 3, 4} };
+	vector<vector<int>> test = { {3, 2, 3},{1, 1, 2},{2, 3, 4} };
+	//vector<vector<int>> test = { {3, 1, 2},{3, 2, 3},{1, 1, 4},{2, 1, 4} };
+	//vector<vector<int>> test = { {3, 2, 3},{1, 1, 2},{2, 3, 4} };
+	int ret = maxNumEdgesToRemove(4, test);
+	cout << "main：ret = " << ret << endl;
+	return 0;
+}
--- letcode/825_Medium.cpp
+++ letcode/825_Medium.cpp
@@ -29,34 +29,106 @@ using namespace std;
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 
-//双指针遍历数组
+////原版：双指针遍历数组
+//int numFriendRequests(vector<int>& ages)
+//{
+//	sort(ages.begin(), ages.end());
+//	vector<int>::reverse_iterator ritJ;
+//	vector<int>::reverse_iterator ritI;
+//	int tmp;
+//	int requestCnt = 0;
+//	for (ritI = ages.rbegin(); ritI != ages.rend(); ritI++)
+//	{
+//		ritJ = ritI + 1;
+//		tmp = *ritI*0.5 + 7;
+//		while (ritJ != ages.rend() && *ritJ > tmp)
+//		{
+//			cout << "numFriendRequests：产生好友请求  " << *ritI << " -> " << *ritJ << endl;
+//			requestCnt++;
+//			if (*ritI == *ritJ) //两人年纪相等时，可以互相发送好友请求
+//				requestCnt++;
+//			ritJ++;
+//		}
+//	}
+//	return requestCnt;
+//}
+
+
+//双指针升级版：双指针遍历双数组。用 lowerbounds 数组记录 A 的适龄好友的年龄下确界。
+//int numFriendRequests(vector<int>& ages)
+//{
+//	sort(ages.begin(), ages.end(), [](int ageA, int ageB) {return ageA > ageB; });
+//	int requestCnt = 0;
+//	int currentBound;
+//	int sameAgeCnt = 0;
+//	vector<int> lowerbounds;
+//	int indexBound = 0;
+//	for (vector<int>::iterator it = ages.begin(); it != ages.end(); it++)
+//	{
+//		if (*it > 14 && it != ages.begin() && *it == *(it - 1)) //计算在之前已判断的人中，与当前人的年龄相等的人数
+//		{
+//			sameAgeCnt++;
+//			requestCnt += sameAgeCnt; //两人年纪相等时，可以互相发送好友请求
+//		}
+//		else
+//			sameAgeCnt = 0;
+//		while (indexBound != lowerbounds.size() && lowerbounds[indexBound] >= *it)
+//		{
+//			indexBound++;
+//		}
+//		requestCnt += (lowerbounds.size() - indexBound);
+//		currentBound = *it*0.5 + 7;
+//		lowerbounds.push_back(currentBound);
+//	}
+//	return requestCnt;
+//}
+
+
+//桶排序再升级版：双指针遍历双数组。用年龄桶同代替排序，用 lowerbounds 数组记录 A 的适龄好友的年龄下确界。
 int numFriendRequests(vector<int>& ages)
 {
-	sort(ages.begin(), ages.end());
-	vector<int>::reverse_iterator ritJ;
-	vector<int>::reverse_iterator ritI;
-	int tmp;
-	int requestCnt = 0;
-	for (ritI = ages.rbegin(); ritI != ages.rend(); ritI++)
+	const int AgeUpperBound = 120;
+	const int BucketSize = AgeUpperBound + 1 - 15;
+	vector<int> ageBucket(BucketSize, 0); //年龄桶，只有年龄段在 [15, 120] 内的人才有朋友
+
+	//装填年龄桶（降序）
+	for (int i = 0; i < ages.size(); i++)
 	{
-		ritJ = ritI + 1;
-		tmp = *ritI*0.5 + 7;
-		while (ritJ != ages.rend() && *ritJ > tmp)
+		if (ages[i] > 14)
+			ageBucket[AgeUpperBound - ages[i]]++; // ageBucket[i] 为年龄为 AgeUpperBound - i 的人数
+	}
+
+	//遍历年龄桶，装填有可能作为 A 的年龄数组，计算好友数量
+	vector<int> ageA; //有可能作为 A 的年龄数组
+	int requestCnt = 0;
+	int indexA = 0;
+	int candidateCnt = 0; //当前 lowerbounds 数组中，有可能作为 A 的人数
+	for (int i = 0; i < BucketSize; i++)
+	{
+		//索引 i 对应的年龄为 AgeUpperBound - i ，人数为 ageBucket[i]
+		if (0 == ageBucket[i])
+			continue;
+		requestCnt += ageBucket[i] * (ageBucket[i] - 1); //年龄相等的人互相发送好友邀请
+		while (indexA != ageA.size() && ageA[indexA] / 2 + 7 >= AgeUpperBound - i) //年龄为 AgeUpperBound - i 的人作为 B 接收好友邀请
 		{
-			cout << "numFriendRequests：产生好友请求  " << *ritI << " -> " << *ritJ << endl;
-			requestCnt++;
-			if (*ritI == *ritJ) //两人年纪相等时，可以互相发送好友请求
-				requestCnt++;
-			ritJ++;
+			candidateCnt -= ageBucket[AgeUpperBound - ageA[indexA]]; //年龄为 ageA[indexA] 的人已不可能作为 A 而发送好友邀请
+			indexA++;
 		}
+		requestCnt += ageBucket[i] * candidateCnt; // ageBucket[i] * candidateCnt 为年龄为 AgeUpperBound - i 的人作为 B 接收到的好友邀请的数量。
+		ageA.push_back(AgeUpperBound - i); //年龄为 AgeUpperBound - i 的人有可能作为 A 而发送好友邀请
+		candidateCnt += ageBucket[i]; //年龄为 AgeUpperBound - i 的人都可能作为 A 而发送好友邀请
 	}
 	return requestCnt;
 }
 
 
-int main()
+int main825()
 {
-	vector<int> test = { 16,16 };
+	//vector<int> test = { 16,17,18 };
+	vector<int> test = { 20,30,37,37,37,37,37 };
+	//vector<int> test = { 20,30,100,110,120 };
+	//vector<int> test = { 73, 106, 6,39,26, 15, 30, 6,6,6,6,6,100, 71, 35, 46, 112, 6, 60, 110 };
 	int ret = numFriendRequests(test);
 	cout << "main：ret = " << ret << endl;
+	return 0;
 }
