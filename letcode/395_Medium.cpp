27215296b35cc976d4ba1c4d1e72c0dbbd02c1e9
lyfhouyi <lyfhouyi@163.com>
2021-02-27 22:20:55 +0800

    houyi

---
+++ letcode/395_Medium.cpp
@@ -0,0 +1,83 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+//给你一个字符串 s 和一个整数 k ，请你找出 s 中的最长子串， 要求该子串中的每一字符出现次数都不少于 k 。返回这一子串的长度。
+//
+//
+//提示：
+//
+//	1 <= s.length <= 10^4
+//	s 仅由小写英文字母组成
+//	1 <= k <= 10^5
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/longest-substring-with-at-least-k-repeating-characters
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+//计算字符串 s 在区间 [begin,end) 上的最长子串，要求该子串中的每一字符出现次数都不少于 k 。
+//递归。首先遍历区间一次，找到所有的“坏字符”，进而划分出若干各候选区间；随后，对每一个候选区间，递归调用 longestSubstringSlice 求各候选区间的满足要求的子串长度的最大值。递归基：若当前区间满足要求，返回当前区间长度。
+int longestSubstringSlice(string& s, int k, int begin, int end)
+{
+	vector<int> charCntBucket(26); //统计各字符出现的次数
+	for (int i = begin; i < end; i++)
+		charCntBucket[s[i] - 'a']++;
+
+	//递归基：若当前区间满足要求，返回当前区间长度。
+	bool isValid = true;
+	for (int i = 0; i < 26; i++)
+	{
+		if (charCntBucket[i] > 0 && charCntBucket[i] < k)
+			isValid = false;
+	}
+	if (isValid)
+		return end - begin;
+
+	//计算各候选区间的满足要求的子串长度的最大值
+	int maxLength = 0;
+	int subBegin = -1;
+	for (int i = begin; i < end; i++)
+	{
+		if (charCntBucket[s[i] - 'a'] >= k && -1 == subBegin) //找到了候选区间的起点
+		{
+			subBegin = i;
+		}
+		else
+		{
+			if (charCntBucket[s[i] - 'a'] < k && -1 != subBegin) //找到了候选区间的终点
+			{
+				if (i - subBegin >= k)
+				{
+					int tmp = longestSubstringSlice(s, k, subBegin, i);
+					maxLength = maxLength > tmp ? maxLength : tmp;
+				}
+				subBegin = -1;
+			}
+		}
+	}
+	if (subBegin != -1 && end - subBegin >= k) //处理最后一个候选区间
+	{
+		int tmp = longestSubstringSlice(s, k, subBegin, end);
+		maxLength = maxLength > tmp ? maxLength : tmp;
+	}
+	return maxLength;
+}
+
+
+//分而治之。递归实现。
+int longestSubstring(string s, int k)
+{
+	return longestSubstringSlice(s, k, 0, s.size());
+}
+
+
+int main395()
+{
+	string s = "bbaaacbd";
+	int ret = longestSubstring(s, 3);
+	cout << "main：ret = " << ret << endl;
+	return 0;
+}
--- letcode/78_Medium.cpp
+++ letcode/78_Medium.cpp
@@ -1,4 +1,10 @@
-﻿//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+﻿#include<iostream>
+#include<vector>
+#include<bitset>
+
+using namespace std;
+
+//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
 //
 //解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
 //
@@ -12,3 +18,133 @@
 //来源：力扣（LeetCode）
 //链接：https ://leetcode-cn.com/problems/subsets
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+void subsetsBegin(vector<vector<int>>& ret, int cur, vector<int>& nums, vector<int>& currentSubSet)
+{
+	if (cur == nums.size())
+	{
+		ret.push_back(currentSubSet);
+		return;
+	}
+	currentSubSet.push_back(nums[cur]);
+	subsetsBegin(ret, cur + 1, nums, currentSubSet);
+	currentSubSet.pop_back();
+	subsetsBegin(ret, cur + 1, nums, currentSubSet);
+}
+
+
+//解法一：位表示法。利用值域有限的特性，将原数组表示为一个 21 位二进制数；通过计算该二进制数的各位的全部子集得到原数组的所有可能的子集；最后将全部子集的位表示法转化为值域上的数值得到所求返回值。
+//vector<vector<int>> subsets(vector<int>& nums) 
+//{
+//	vector<vector<int>> ret;
+//	int n = nums.size();
+//	//用一个 21 位二进制数表示原数组  由高至低各位分别表示 10,9,8,...,1,0,-1,-2,-3,...,-10
+//	int setMask=0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		setMask |= 1 << nums[i] + 10;
+//	}
+//	//cout <<"setMask = "<< bitset<21>(setMask) << endl;
+//	int subSetBit = setMask;
+//	int tmp;
+//	int pos;
+//	while (subSetBit != 0) //计算原数组的子集
+//	{
+//		//此时 subSetBit 是原数组的一个子集的位表示
+//		tmp = subSetBit;
+//		pos = 0;
+//		vector<int> subSet;
+//		while (tmp != 0) //将位表示的子集转化为值域上的数值
+//		{
+//			if (0 != (tmp & 1))
+//			{
+//				subSet.push_back(pos - 10);
+//			}
+//			pos++;
+//			tmp >>= 1;
+//		}
+//		ret.push_back(subSet);
+//		subSetBit = (subSetBit - 1) & setMask;
+//	}
+//	ret.push_back(vector<int>());
+//	return ret;
+//}
+
+
+//解法二：迭代。遍历原数组中各元素，每次将新的元素加入到当前的各集合中构成新的子集。
+//vector<vector<int>> subsets(vector<int>& nums)
+//{
+//	int n = nums.size();
+//	vector<vector<int>> ret(1, vector<int>()); //初始状态：只有一个空集
+//	for (int i = 0; i < n; i++) //将新的元素加入到当前的各集合中构成新的子集
+//	{
+//		int len = ret.size();
+//		for (int j = 0; j < len; j++)
+//		{
+//			ret.push_back(ret[j]);
+//			ret[len + j].push_back(nums[i]);
+//		}
+//	}
+//	return ret;
+//}
+
+
+//解法三：二进制枚举。对原数组中的每个元素，子集中可能包含它也可能不包含它；对原数组中的任一元素，用 1 表示某子集中包含它，则枚举原数组中每个元素包含与否的全部可能情况后，将用 0/1 表示的子集转化为用元素数值表示的子集即可。
+//vector<vector<int>> subsets(vector<int>& nums)
+//{
+//	int n = nums.size();
+//	vector<vector<int>> ret;
+//	int setMask = pow(2, n) - 1; //原数组的 0/1 表示法
+//	int subSetBit = setMask; //子集的 0/1 表示法
+//	vector<int> subSet; //子集的元素数值表示法
+//	int tmp;
+//	int pos;
+//	while (subSetBit != 0)
+//	{
+//		subSet.clear();
+//		tmp = subSetBit;
+//		pos = 0;
+//		while (tmp != 0) //将位表示的子集转化为值域上的数值
+//		{
+//			if (0 != (tmp & 1))
+//			{
+//				subSet.push_back(nums[pos]);
+//			}
+//			pos++;
+//			tmp >>= 1;
+//		}
+//		ret.push_back(subSet);
+//		subSetBit = (subSetBit - 1) & setMask;
+//	}
+//	ret.push_back(vector<int>());
+//	return ret;
+//}
+
+
+//解法四：回溯算法。遍历原数组，使用递归分别处理子集包含当前元素及不包含当前元素的情况。
+vector<vector<int>> subsets(vector<int>& nums)
+{
+	int n = nums.size();
+	vector<vector<int>> ret;
+	vector<int> currentSubSet;
+	subsetsBegin(ret, 0, nums, currentSubSet);
+	return ret;
+}
+
+
+int main78()
+{
+	//vector<vector<int>> test = { {3} };
+	//vector<vector<int>> test = { {1,2,3},{4,5,6} };
+	vector<int> test = { 1,2,3 };
+	vector<vector<int>> ret = subsets(test);
+	cout << "main：ret =" << endl;
+	for (vector<vector<int>>::iterator it = ret.begin(); it != ret.end(); it++)
+	{
+		for (vector<int>::iterator it2 = it->begin(); it2 != it->end(); it2++)
+			cout << *it2 << " ";
+		cout << endl;
+	}
+	return 0;
+}
