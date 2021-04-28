57ca5a03eb55add660f6a5448858870ef4d345c6
lyfhouyi <lyfhouyi@163.com>
2021-04-28 15:52:17 +0800

    houyi

---
+++ letcode/240_Medium.cpp
@@ -0,0 +1,67 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+//
+//每行的元素从左到右升序排列。
+//每列的元素从上到下升序排列。
+//
+//
+//提示：
+//
+//	m == matrix.length
+//	n == matrix[i].length
+//	1 <= n, m <= 300
+//	- 10^9 <= matix[i][j] <= 10^9
+//	每行的所有元素从左到右升序排列
+//	每列的所有元素从上到下升序排列
+//	- 10^9 <= target <= 10^9
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/search-a-2d-matrix-ii
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+//解法一：贪心算法。基于贪心的思路，每次查找都可排除矩阵的一行或一列，直到找到目标值；若查找完毕时仍未发现目标值，则目标值必不存在。
+//bool searchMatrix(vector<vector<int>>& matrix, int target)
+//{
+//	int m = matrix.size();
+//	int n = matrix[0].size();
+//	int i = 0;
+//	int j = n - 1;
+//	while (i < m && j >= 0)
+//	{
+//		if (matrix[i][j] < target)
+//			i++;
+//		else if (matrix[i][j] > target)
+//			j--;
+//		else
+//			return true;
+//	}
+//	return false;
+//}
+
+
+//解法二：二分查找。遍历矩阵各行，利用各行元素的有序性，使用二分查找法判断当前行中是否包含目标值。
+bool searchMatrix(vector<vector<int>>& matrix, int target)
+{
+	int m = matrix.size();
+	for (int i = 0; i < m; i++)
+	{
+		if (binary_search(matrix[i].begin(), matrix[i].end(), target))
+			return true;
+	}
+	return false;
+}
+
+
+int main240()
+{
+	vector<vector<int>> test = { {1, 4, 7, 11, 15} ,{2, 5, 8, 12, 19},{3, 6, 9, 16, 22},{10, 13, 14, 17, 24},{18, 21, 23, 26, 30} };
+	bool ret = searchMatrix(test, 30);
+	cout << "main：ret = " << ret << endl;
+	return 0;
+}
---
+++ letcode/633_Medium.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+
+using namespace std;
+
+//给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。
+//
+//
+//提示：
+//
+//	0 <= c <= 2^31 - 1
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/sum-of-square-numbers
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+//解法一：暴力解法。逐项遍历整数 a ，判断其是否满足要求。
+//bool judgeSquareSum(int c)
+//{
+//	for (int a = 0; a*a <= c / 2; a++)
+//	{
+//		int b2 = c - a * a;
+//		if (sqrt(b2) == (int)sqrt(b2))
+//			return true;
+//	}
+//	return false;
+//}
+
+
+//解法二：双指针。使用双指针分别指向候选的整数 a、b ；若当前平方和小于 c ，则将 a++ ；若当前平方和大于 c ，则将 b-- ；若当前平方和等于 c ，则找到答案。双指针分别从 [0,sqrt(c)] 开始向中间靠拢，直到 a > b 时退出循环。
+bool judgeSquareSum(int c)
+{
+	long a = 0;
+	long b = sqrt(c);
+	while (a <= b)
+	{
+		if (a * a + b * b < c)
+			a++;
+		else if (a * a + b * b > c)
+			b--;
+		else
+			return true;
+	}
+	return false;
+}
+
+int main633()
+{
+	bool ret = judgeSquareSum(4);
+	cout << "main：ret = " << ret << endl;
+	return 0;
+}
